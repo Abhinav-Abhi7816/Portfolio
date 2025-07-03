e3a53f37d0452616e2e4046ee40b8e5af4e23a0c
ABHINAV ABHI <Abhinav Thonti>
2025-07-03 20:40:21 +0530

    fixed the size of card

--- src/Components/SkillSec.js
+++ src/Components/SkillSec.js
@@ -6,7 +6,7 @@ function SkillSec() {
       <section className="skills md:pt-5 font-bold">
         <h1>My Technical Skills</h1>
         <div className="flex justify-around  flex-wrap gap-12 px-5 md:py-12 py-8 ">
-            <h2>Java</h2>
+            <h2>Java (DSA)</h2>
             <h2>JavaScript</h2>
             <h2>HTML/CSS</h2>
             <h2>React-JS</h2>
